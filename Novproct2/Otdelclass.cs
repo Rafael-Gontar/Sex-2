0b77b2540deaa4bbb53df1c347448db270e0ffde
C model <rafa37567@gmail.com>
2024-03-17 20:38:29 +0100

    2 kommit

---
+++ Novproct2/Otdelclass.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Novproct2
+{
+    public class Otdelclass
+    {
+        public static void Maee(string[] args)
+        {
+            Console.WriteLine("hallo nika");
+        }
+    } 
+}
