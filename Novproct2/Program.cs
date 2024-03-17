b42c7173e9f0f279648e12459efddaee5b8114a0
C model <rafa37567@gmail.com>
2024-03-17 20:15:03 +0100

    1 commit

---
+++ Novproct2/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Novproct2
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("hallo artur");
+        }
+    }
+}
